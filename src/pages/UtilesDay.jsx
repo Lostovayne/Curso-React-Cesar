09fc8b473da7236bab3a3481fdb2347190239c9b
Lostovayne <frankhx0_18@hotmail.com>
2023-02-26 13:29:36 -0300

    Agregado nuevo material Helpers

--- src/pages/UtilesDay.jsx
+++ src/pages/UtilesDay.jsx
@@ -1,11 +1,27 @@
 import {Link} from 'react-router-dom'
-
-
+import dayjs from "dayjs"
+import {es} from "dayjs/locale/es"
 
 
 const UtilesDay = () => {
+  
+  let fecha = new Date()
+  
   return (
-    <div>UtilesDay</div>
-  )
+    <>
+      <h1>Ejemplos de la libreria Dayjs</h1>
+
+      <h2>Formatear Fecha</h2>
+      <ul>
+        <li>Fecha: {fecha.toString()}</li>
+        <li>
+          Fecha: {dayjs(fecha).locale('es').format('dddd').toUpperCase()}{' '}
+          {dayjs(fecha).format('DD')} de{' '}
+          {dayjs(fecha).locale('es').format('MMMM').toUpperCase()} del{' '}
+          {dayjs(fecha).format('YYYY')} || {dayjs(fecha).format('DD/MM/YYYY')}{' '}
+        </li>
+      </ul>
+    </>
+  );
 }
 export default UtilesDay
